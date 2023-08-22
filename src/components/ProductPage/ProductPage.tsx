2e5ae499cbe847bbf06adde71c8b0c7772b4eff3
shailendrayadav7 <sailendrayadav80@gmail.com>
2023-08-22 14:51:52 -0500

    Init Commit

---
+++ src/components/ProductPage/ProductPage.tsx
@@ -0,0 +1,36 @@
+
+
+import React from 'react';
+import { useFetchProductsQuery } from '../../services/products'; 
+import CardComponent from '../CardComponent/CardComponent';
+
+
+const ProductPage: React.FC = () => {
+  const { data: products, isLoading, isError } = useFetchProductsQuery();
+
+  if (isLoading) {
+    return <div>Loading...</div>; 
+  }
+
+  if (isError) {
+    return <div>Error fetching products.</div>; 
+  }
+
+  // Add a nullish check for the 'products' variable
+  if (!products) {
+    return <div>No products available.</div>;
+  }
+
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <div>
+        {products.map((product) => (
+          <CardComponent key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
---
+++ src/store/store.ts
@@ -0,0 +1,17 @@
+
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query/react';
+import { productsApi } from '../services/products'; 
+
+const store = configureStore({
+  reducer: {
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
+
+// RTK Query listeners
+setupListeners(store.dispatch);
+
+export default store;
