261b581e89c78c7fed9a0f0caf57be89e6ffab56
shailendrayadav7 <sailendrayadav80@gmail.com>
2023-08-23 23:23:43 -0500

    Add Home Component

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,19 +1,18 @@
-
+// ProductPage.tsx
 
 import React from 'react';
 import { useFetchProductsQuery } from '../../services/products'; 
 import CardComponent from '../CardComponent/CardComponent';
 
-
 const ProductPage: React.FC = () => {
-  const { data: products, isLoading, isError } = useFetchProductsQuery();
+  const { data: products, isLoading, isError } = useFetchProductsQuery(); 
 
   if (isLoading) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
 
   if (isError) {
-    return <div>Error fetching products.</div>; 
+    return <div>Error fetching products.</div>;
   }
 
   // Add a nullish check for the 'products' variable
