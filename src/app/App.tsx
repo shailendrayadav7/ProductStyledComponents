7288e059437b34c00d5f8b83d519375bf41d8e6b
shailendrayadav7 <sailendrayadav80@gmail.com>
2023-08-24 22:36:56 -0500

    Endpoint working fine

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,44 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import ProductPage from '../components/ProductPage/ProductPage';
+// import ProductDetailPage from '../pages/ProductDetailPage';
+// import '../App.css';
+
+// // Define a simple Home component
+// const Home: React.FC = () => {
+//   return <div>Welcome to the Home Page!</div>;
+// };
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <nav className="p-4 bg-primary">
+//           <div className="container mx-auto flex justify-between items-center">
+//             <Link to="/" className="text-white text-2xl font-semibold">
+//               Redux Query App
+//             </Link>
+//             <Link to="/products" className="text-white">
+//               Products
+//             </Link>
+//           </div>
+//         </nav>
+
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/products" element={<ProductPage />} />
+//           <Route path="/products/:productId" element={<ProductDetailPage />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ProductPage from '../components/ProductPage/ProductPage';
@@ -11,27 +52,26 @@ const Home: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="App">
-        <nav className="p-4 bg-primary">
-          <div className="container mx-auto flex justify-between items-center">
-            <Link to="/" className="text-white text-2xl font-semibold">
-              Redux Query App
-            </Link>
-            <Link to="/products" className="text-white">
-              Products
-            </Link>
-          </div>
-        </nav>
+    <div className="App">
+      <nav className="p-4 bg-primary">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link to="/" className="text-white text-2xl font-semibold">
+            Redux Query App
+          </Link>
+          <Link to="/products" className="text-white">
+            Products
+          </Link>
+        </div>
+      </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductPage />} />
-          <Route path="/products/:productId" element={<ProductDetailPage />} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/products/:productId" element={<ProductDetailPage />} />
+      </Routes>
+    </div>
   );
 };
 
 export default App;
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,33 @@
-import ReactDOM from 'react-dom';
-import App from './app/App';
-import './index.css';
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux'; // Import the Provider
+// import { BrowserRouter } from 'react-router-dom';
+// import { createRoot } from 'react-dom/client';
+// import App from './app/App';
+// import store from './store/store'; // Import your Redux store src\store\store.ts
+// import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+// createRoot(document.getElementById('root')!).render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+// );
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Make sure to import BrowserRouter
+
+import App from './app/App'; // Adjust the import path as needed
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
+
+
